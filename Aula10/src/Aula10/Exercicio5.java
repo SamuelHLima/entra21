1e0c80c8ae6439f2b3521d9f9d1435a68444af2b
SamuelHLima <115194609+SamuelHLima@users.noreply.github.com>
2023-09-11 18:46:11 -0300

    Exercicio5.java

--- Aula10/src/Aula10/Exercicio5.java
+++ Aula10/src/Aula10/Exercicio5.java
@@ -8,7 +8,45 @@ public class Exercicio5 {
 
 Imprima quais valores desses informados são maiores que a média dos valores.
 		 */
+package controle_candidatos;
 
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		/*
+		 * Descreva um algoritmo que leia 12 valores reais e os coloque em um vetor de 12 posições do tipo real. 
+
+Imprima quais valores desses informados são maiores que a média dos valores.
+		 */
+		
+		double[] valores=new double [12];
+		double valor=0;
+		
+		double media;
+		for (int i = 0; i < valores.length; i++) {
+			System.out.println("Digite 12 elementos");
+			valores[i]= teclado.nextDouble();
+			valor += valores[i];
+			
+				
+			}
+			media = valor/12;
+			for (int i = 0; i < valores.length; i++) {
+				
+			if (valores[i]>media) {
+				System.out.println(valores[i]);
+			}
+		
+		 
+	
+		
+		
+	}
+	}
+}
 	}
 
 }
